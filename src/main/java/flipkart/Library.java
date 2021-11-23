6bbeecfba4e34159b54ac5eb14b6544a52efa2a9
Kiran <kirannet216@gmail.com>
2021-11-23 08:57:14 +0530

    local

---
+++ src/main/java/flipkart/Library.java
@@ -0,0 +1,11 @@
+package flipkart;
+
+import android.content.Context;
+
+public class Library {
+
+    public static String getHelloMessage(Context context) {
+        return context.getString(R.string.hello);
+    }
+
+}
